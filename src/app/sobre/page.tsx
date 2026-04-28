79421c94951cde20c22fc42ec2127f51f82fde00
yoanny <manuelvaal2021@gmail.com>
2026-04-28 15:53:11 +0100

    add sobre page animation

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,8 +1,74 @@
+"use client"
 import Container from "@/components/layout/Container";
 import LeadershipSection from "@/components/ui/LeaderCard";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
+import { useRef } from "react";
 
 export default function SobrePage() {
+  const textRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLDivElement>(null);
+  const cardsRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const tl = gsap.timeline();
+ 
+    tl.from(".title",{y:30,opacity:0,ease:"power3.inOut",duration:0.8})
+      .from(".subtitle",{y:30,opacity:0,ease:"power3.inOut",duration:0.5},">-0.5")
+      .from(textRef.current, {
+         y: 50,
+         opacity: 0.5,
+         duration: 1,
+         ease: "power3.out",
+         scrollTrigger: {
+           trigger: textRef.current,
+           start: "top 80%",
+           scrub:2,
+         },
+       })
+       .from(imageRef.current, {
+         x: 150,
+         opacity: 0,
+         scale:0.95,
+         duration: 1,
+         delay: 0.2,
+         ease: "power3.out",
+         scrollTrigger: {
+           trigger: imageRef.current,
+           start: "top 80%",
+           scrub:2
+         },
+       });
+
+      if (cardsRef.current) {
+       const cards = Array.from(cardsRef.current.children);
+
+      gsap.fromTo(
+        cards,
+        {
+          y: 30,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.8,
+          ease: "power3.out",
+          stagger: 0.2,
+          scrollTrigger: {
+            trigger: cardsRef.current,
+            start: "top 85%",
+            toggleActions: "play none none none",
+            markers: true,
+          },
+        }
+      );
+  }
+},[]);
+
   return (
     <main className="bg-zinc-950 text-white">
       
@@ -13,11 +79,11 @@ export default function SobrePage() {
             Sobre Nós
           </p>
 
-          <h1 className="mt-4 text-4xl md:text-5xl font-bold">
+          <h1 className="title mt-4 text-4xl md:text-5xl font-bold">
             Construindo o futuro da indústria diamantífera
           </h1>
 
-          <p className="mt-6 text-zinc-400 max-w-2xl mx-auto">
+          <p className="subtitle mt-6 text-zinc-400 max-w-2xl mx-auto">
             Uma empresa angolana focada em inovação, sustentabilidade
             e parcerias estratégicas globais.
           </p>
@@ -28,7 +94,9 @@ export default function SobrePage() {
         <Container>
           <div className="grid md:grid-cols-2 gap-12 items-center">
             
-            <div className="text-justify hyphens-auto">
+            <div
+              ref={textRef}
+              className="text-justify hyphens-auto">
               <h2 className="text-3xl text-zinc-950 font-semibold">
                 A nossa história
               </h2>
@@ -43,7 +111,9 @@ export default function SobrePage() {
               </p>
             </div>
 
-            <div className="relative h-95 bg-zinc-200 rounded-xl border border-zinc-300 overflow-hidden">
+            <div 
+             ref={imageRef}
+             className="relative h-95 bg-zinc-200 rounded-xl border border-zinc-300 overflow-hidden">
               <Image
                 src="/images/PR_PCA.webp"
                 alt="PCA"
@@ -58,9 +128,10 @@ export default function SobrePage() {
         </Container>
       </section>
 
-      <section className="py-24 bg-zinc-900/40">
+      <section className="py-24 bg-zinc-900/40" >
         <Container>
-          <div className="grid md:grid-cols-3 gap-8">
+          <div ref={cardsRef}
+             className="grid md:grid-cols-3 gap-8">
             
             {[
               {
@@ -79,7 +150,7 @@ export default function SobrePage() {
               <div
                 key={i}
                 className="p-6 border border-zinc-800 rounded-xl bg-zinc-800/50 backdrop-blur-sm
-                shadow-sm shadow-yellow-500 hover:shadow-md hover:scale-110 transition duration-500"
+                shadow-sm shadow-yellow-500 hover:shadow-md hover:scale-[1.03] transition duration-500"
               >
                 <h3 className="text-lg font-medium text-yellow-400">
                   {item.title}
